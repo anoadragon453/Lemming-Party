a5b5fea697d6750caaf244f48370a006b1185ae1
Abhishyant Khare <Abhishyant@Abhishyants-Mac.local>
2014-11-08 18:43:59 -0800

    buggy sand

--- Lemming-Party/GameScene.h
+++ Lemming-Party/GameScene.h
@@ -12,4 +12,5 @@
 
 @end
 
-NSMutableArray *lemmingArray;
+NSMutableArray *sandParticles ;
+BOOL stillHolding;
--- Lemming-Party/GameScene.m
+++ Lemming-Party/GameScene.m
@@ -12,31 +12,84 @@
 
 -(void)didMoveToView:(SKView *)view {
     /* Setup your scene here */
- 
-    
+    [[self physicsWorld] setGravity:CGVectorMake(0.0, -2.0)];
+    sandParticles = [[NSMutableArray alloc] init] ;
+    stillHolding = true;
     
 }
 
 -(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
     /* Called when a touch begins */
+  
+    for (UITouch *touch in touches) {
+        CGPoint location = [touch locationInNode:self];
+        for(int i = 0; i <3; i++){
+            
+            
+            SKSpriteNode *sand = [SKSpriteNode spriteNodeWithImageNamed:@"sand-particle"];
+            
+            sand.xScale = 2.0;
+            sand.yScale = 2.0;
+            float x = location.x+ (float)(2i);
+            float y = location.y + (float)(i);
+            sand.position = CGPointMake(x, y);
+            sand.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:sand.size];
+            
+            [sandParticles addObject:sand];
+            [self addChild:sand];
+    }
+        [self performSelector:@selector(longPress:) withObject:[NSValue valueWithCGPoint:location] afterDelay:1.0];
+    }
+   
+    
+}
+-(void)touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event{
     
     for (UITouch *touch in touches) {
         CGPoint location = [touch locationInNode:self];
+        for(int i = 0; i <3; i++){
+            
         
         SKSpriteNode *sand = [SKSpriteNode spriteNodeWithImageNamed:@"sand-particle"];
         
         sand.xScale = 2.0;
         sand.yScale = 2.0;
-        sand.position = location;
+            float x = location.x+ (float)(2i);
+            float y = location.y + (float)(i);
+            sand.position = CGPointMake(x, y);
+        sand.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:sand.size];
         
-        
-    
+        [sandParticles addObject:sand];
         [self addChild:sand];
+        }
+        [NSObject cancelPreviousPerformRequestsWithTarget:self selector:@selector(longPress:) object:[NSValue valueWithCGPoint:location]];
+        
     }
-    
 }
 -(void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event{
+     for (UITouch *touch in touches) {
+          CGPoint location = [touch locationInNode:self];
+         [NSObject cancelPreviousPerformRequestsWithTarget:self selector:@selector(longPress:) object:[NSValue valueWithCGPoint:location]];
+     }
     
+}
+-(void) longPress: (NSValue *) locationVal{
+    for(int i = 0; i <3; i++){
+        
+        
+        SKSpriteNode *sand = [SKSpriteNode spriteNodeWithImageNamed:@"sand-particle"];
+        CGPoint location = [locationVal CGPointValue];
+        sand.xScale = 2.0;
+        sand.yScale = 2.0;
+        float x = location.x+ (float)(2i);
+        float y = location.y + (float)(i);
+        sand.position = CGPointMake(x, y);
+        sand.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:sand.size];
+        
+        [sandParticles addObject:sand];
+        [self addChild:sand];
+    }
+
 }
 
 -(void)update:(CFTimeInterval)currentTime {
