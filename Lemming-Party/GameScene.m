06853ab2555184b630bdbcd2c9ee01e7945876c1
Andrew Morgan <anoadragon453@gmail.com>
2014-11-08 17:59:44 -0800

    My stuff.

--- Lemming-Party/GameScene.m
+++ Lemming-Party/GameScene.m
@@ -12,9 +12,39 @@
 
 -(void)didMoveToView:(SKView *)view {
     /* Setup your scene here */
- 
     
+    // Get screen size and height
+    CGRect screenRect = [[UIScreen mainScreen] bounds];
+    CGFloat screenWidth = screenRect.size.width;
+    CGFloat screenHeight = screenRect.size.height;
     
+    // Create the background
+    SKSpriteNode *background = [SKSpriteNode spriteNodeWithImageNamed:@"sky.png"];
+    background.position = CGPointMake(360, 320);
+    [self addChild:background];
+    
+    // Create the floor
+    SKSpriteNode *floor = [SKSpriteNode spriteNodeWithImageNamed:@"platform.png"];
+    floor.position = CGPointMake(0, 80);
+    floor.anchorPoint = CGPointMake(0, 0);
+    [self addChild:floor];
+    
+    // SEND IN THE LEMMINGS!!!
+    [self createAmountOfLemmings:10];
+}
+
+-(void)createAmountOfLemmings:(int)count {
+    for (int i = 0; i < count; i++) {
+        // Create the lemming sprite node
+        SKSpriteNode *lemming = [SKSpriteNode spriteNodeWithImageNamed:@"lemming.png"];
+        lemming.position = CGPointMake(200, 300);
+        [lemmingArray addObject:lemming];
+        
+        
+        
+        [self addChild:lemming];
+        
+    }
 }
 
 -(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
@@ -23,20 +53,18 @@
     for (UITouch *touch in touches) {
         CGPoint location = [touch locationInNode:self];
         
-        SKSpriteNode *sand = [SKSpriteNode spriteNodeWithImageNamed:@"sand-particle"];
+        SKSpriteNode *sprite = [SKSpriteNode spriteNodeWithImageNamed:@"Spaceship"];
         
-        sand.xScale = 2.0;
-        sand.yScale = 2.0;
-        sand.position = location;
+        sprite.xScale = 0.5;
+        sprite.yScale = 0.5;
+        sprite.position = location;
         
-        // eit
-    
-        [self addChild:sand];
+        SKAction *action = [SKAction rotateByAngle:M_PI duration:1];
+        
+        [sprite runAction:[SKAction repeatActionForever:action]];
+        
+        [self addChild:sprite];
     }
-    
-}
--(void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event{
-    
 }
 
 -(void)update:(CFTimeInterval)currentTime {
